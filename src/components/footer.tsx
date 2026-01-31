8598251064f3d4132cf332731e733d851a9be6b2
Eduardo Delgaldo <43506614+upwiththecomputers@users.noreply.github.com>
2026-01-31 20:08:33 +0100

    refactor: move next project to root

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer class="footer">
-      <div class="footerInner">
-        <p class="copyright">&copy; 2025 Hanna Mikulska-Delgaldo</p>
-      </div>
-    </footer>
-  );
-}
